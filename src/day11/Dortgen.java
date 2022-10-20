110f45774082fc425469b84bb2ff1a7812f5a9bc
reyhanacar <reyhanozkanacar@gmail.com>
2022-10-20 13:48:27 +0300

    Reyhan ACAR- has been completed.

--- src/day11/Dortgen.java
+++ src/day11/Dortgen.java
@@ -1,8 +1,10 @@
 package day11;
 
+import java.util.Scanner;
+
 public class Dortgen {
     public static void main(String[] args) {
-        //TODO: Dortgen programını geliştiriniz.
+
         /*
             1. Klavyeden 3 adet girdi alınacaktır.
                 kenar:int       -> Karenin kenarı
@@ -17,5 +19,42 @@ public class Dortgen {
             6. Elde edilen toplam alan hesaplanıp ekranda gösterilecektir.
             7. Elde edilen toplam çevre ekranda gösterilecektir.
          */
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Karenin kenarını giriniz : ");
+        int squareLenght = input.nextInt();
+        System.out.println("Dikdörtgenin kısa kenarını giriniz : ");
+        int rectShortLenght = input.nextInt();
+        System.out.println("Dikdörtgenin uzun kenarını giriniz :");
+        int rectLongLenght = input.nextInt();
+
+       int toplamAlan= alanHesapla(squareLenght)+alanHesapla(rectLongLenght,rectShortLenght);
+        System.out.println("Alanlar toplamı : "+toplamAlan);
+
+        int toplamCevre=cevreHesapla(squareLenght)+cevreHesapla(rectLongLenght,rectShortLenght);
+        System.out.println("Çevreler Toplamı : " +toplamCevre);
+
+
+
     }
+
+    public static int alanHesapla(int a) {
+        return a * a;
+    }                                               //Overloading yaparak çözmeye çalıştım :)
+
+    public static int alanHesapla(int a, int b) {
+        return a * b;
+    }
+
+    public static int cevreHesapla(int a) {
+        return a * 4;
+    }
+
+
+    public static int cevreHesapla(int a, int b) {
+        return (a + b) * 2;
+
+
+    }
+
 }
