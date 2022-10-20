9d42ae027253d5d1b92ce249808e244f8db1419a
engin hereser <enginhereser@gmail.com>
2022-10-20 12:46:30 +0200

    Hausaufgaben wurde gemacht

--- src/day11/Dortgen.java
+++ src/day11/Dortgen.java
@@ -1,5 +1,7 @@
 package day11;
 
+import java.util.Scanner;
+
 public class Dortgen {
     public static void main(String[] args) {
         //TODO: Dortgen programını geliştiriniz.
@@ -17,5 +19,33 @@ public class Dortgen {
             6. Elde edilen toplam alan hesaplanıp ekranda gösterilecektir.
             7. Elde edilen toplam çevre ekranda gösterilecektir.
          */
+
+
+        Scanner input=new Scanner(System.in);
+        System.out.println("Karenin kenarini giriniz");
+        int kenar= input.nextInt();
+        System.out.println("Diktörtgen icin kisa kenari girin");
+        int kisaKenar= input.nextInt();
+        System.out.println("Diktörgen icin uzun kenari girin");
+        int uzunKenar=input.nextInt();
+        int kareninAlani=alanHesapla(kenar);
+        int diktortgeninAlani=alanHesapla(kisaKenar,uzunKenar);
+        int kareninCevreHesapla=cevreHesapla(kenar);
+        int diktortgeninCevreHesapla=cevreHesapla(kisaKenar,uzunKenar);
+        System.out.println("Toplam alan = "+(kareninAlani+diktortgeninAlani));
+        System.out.println("Toplam cevre = "+(kareninCevreHesapla+diktortgeninCevreHesapla));
+    }
+
+    public static int alanHesapla(int k1,int k2){
+        return k1*k2;
+    }
+    public static int alanHesapla(int k){
+        return k*k;
+    }
+    public static int cevreHesapla(int k){
+        return 4*k;
+    }
+    public static int cevreHesapla(int k1,int k2){
+        return (k1+k2)*2;
     }
 }
