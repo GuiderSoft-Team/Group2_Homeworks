1b0a1160e9bb6812cc60b688cbed05ec25951db8
Mustafa ASLAN <mmustafaslann1453@gmail.com>
2022-10-20 14:08:45 +0300

    Mustafa Aslan tarafından ödev yapıldı.

--- src/day11/Dortgen.java
+++ src/day11/Dortgen.java
@@ -1,5 +1,7 @@
 package day11;
 
+import java.util.Scanner;
+
 public class Dortgen {
     public static void main(String[] args) {
         //TODO: Dortgen programını geliştiriniz.
@@ -17,5 +19,58 @@ public class Dortgen {
             6. Elde edilen toplam alan hesaplanıp ekranda gösterilecektir.
             7. Elde edilen toplam çevre ekranda gösterilecektir.
          */
+
+          alanHesapla();
+          cevreHesapla();
+
+
+
+
+
     }
+    public static void alanHesapla(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Dikdörtgenin kısa kenarını giriniz : ");
+        int k1 = scan.nextInt();
+        System.out.println("Dikdörtgenin uzun kenarını giriniz : ");
+        int k2 = scan.nextInt();
+        System.out.println("Karenin kenarını girniz : ");
+        int kenar = scan.nextInt();
+
+
+        System.out.println("Dikdörtgenin Alanı :"+k1*k2);
+        System.out.println("karenin Alanı : "+kenar*kenar);
+
+
+
+    }
+    public static void cevreHesapla(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Dikdörtgenin kısa kenarını giriniz : ");
+        int k1 = scan.nextInt();
+        System.out.println("Dikdörtgenin uzun kenarını giriniz : ");
+        int k2 = scan.nextInt();
+        System.out.println("Karenin kenarını girniz : ");
+        int kenar = scan.nextInt();
+
+        System.out.println("Dikdörtgenin çevresi : "+2*(k1+k2));
+        System.out.println("Karenin çevresi :" +5*kenar);
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
