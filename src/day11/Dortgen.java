21deb63a1a36b51734a9698dbf3e6e7b8c948aaa
özge topal <the.plus.fineart@gmail.com>
2022-10-20 12:08:58 +0300

    ozge Topal

--- src/day11/Dortgen.java
+++ src/day11/Dortgen.java
@@ -1,5 +1,7 @@
 package day11;
 
+import java.util.Scanner;
+
 public class Dortgen {
     public static void main(String[] args) {
         //TODO: Dortgen programını geliştiriniz.
@@ -17,5 +19,22 @@ public class Dortgen {
             6. Elde edilen toplam alan hesaplanıp ekranda gösterilecektir.
             7. Elde edilen toplam çevre ekranda gösterilecektir.
          */
-    }
-}
+        Scanner input=new Scanner(System.in);
+        System.out.print("lütfen karenin kenarını giriniz: ");
+        int kareK=input.nextInt();
+        System.out.print("lütfen dikdörtgenin kısa kenarını giriniz: ");
+        int dikKk=input.nextInt();
+        System.out.print("lütfen dikdörtgenin uzun kenarını giriniz: ");
+        int dikUk=input.nextInt();
+                int alanlarTop=alanHesapla(kareK,kareK)+alanHesapla(dikKk,dikUk);
+                int cevreTop=cevreHesapla(kareK,kareK)+cevreHesapla(dikUk,dikKk);
+        System.out.println("kare ve dikdortgenin alanları toplamı:  "+alanlarTop);
+        System.out.println("kare ve dikdortgenn cevreleri toplamı:   "+cevreTop);
+        }
+        public static int alanHesapla(int k, int k1){
+        return k*k1;
+        }
+        public static int cevreHesapla(int k,int k1){
+        return (k+k1)*2 ;
+        }
+        }
