1272db2006830b0f0cd8c84cf5f81bfb4051d480
isakrc52 <114700918+isakrc52@users.noreply.github.com>
2022-10-20 18:17:55 +0300

    Dikdörgen Alan ve Çevre Toplamları

--- src/day11/Dortgen.java
+++ src/day11/Dortgen.java
@@ -1,5 +1,7 @@
 package day11;
 
+import java.util.Scanner;
+
 public class Dortgen {
     public static void main(String[] args) {
         //TODO: Dortgen programını geliştiriniz.
@@ -17,5 +19,33 @@ public class Dortgen {
             6. Elde edilen toplam alan hesaplanıp ekranda gösterilecektir.
             7. Elde edilen toplam çevre ekranda gösterilecektir.
          */
-    }
+    Scanner input= new Scanner(System.in);
+        System.out.print("Karenin Bir Kenarını Giriniz: ");
+    int kareKenar = input.nextInt();
+        System.out.print("Dikdörgenin Kısa Kenarını Giriniz: ");
+        int kisaKenar= input.nextInt();
+        System.out.print("Dikdörtgenin Uzun Kenarını Giriniz: ");
+        int uzunKenar= input.nextInt();
+
+        int karealan= alanHesapla(kareKenar,kareKenar);
+        int dkalan= alanHesapla(kisaKenar,uzunKenar);
+        System.out.println("Alanların Toplamı: "+ (karealan+dkalan));
+
+        int kareCevre= cevreHesapla(kareKenar,kareKenar);
+        int dkCevre= cevreHesapla(kisaKenar,uzunKenar);
+
+        System.out.println("Çevrelerin Toplamı: "+(kareCevre+dkCevre));
+
 }
+    public static int alanHesapla ( int k1, int k2) {
+
+        return k1*k2;
+
+    }
+
+public static int cevreHesapla (int k1, int k2)
+{
+    return 2*(k1+k2);
+}
+    }
+
