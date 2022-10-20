a3acb4897d571d966aa9d80e2e457d1d04aac52a
Burcu Uğur <burcuturan737@gmail.com>
2022-10-20 15:52:55 +0300

    Burcu Uğur - Homework is done.

--- src/day11/Dortgen.java
+++ src/day11/Dortgen.java
@@ -1,8 +1,9 @@
 package day11;
 
+import java.util.Scanner;
+
 public class Dortgen {
     public static void main(String[] args) {
-        //TODO: Dortgen programını geliştiriniz.
         /*
             1. Klavyeden 3 adet girdi alınacaktır.
                 kenar:int       -> Karenin kenarı
@@ -17,5 +18,36 @@ public class Dortgen {
             6. Elde edilen toplam alan hesaplanıp ekranda gösterilecektir.
             7. Elde edilen toplam çevre ekranda gösterilecektir.
          */
+
+        Scanner input= new Scanner(System.in);
+
+        System.out.print("Karenin kenarını giriniz: ");
+        int kenar=input.nextInt();
+        System.out.print("Dikdörtgenin kısa kenarını giriniz: ");
+        int kisaKenar=input.nextInt();
+        System.out.print("Dikdörtgenin uzun kenarını giriniz: ");
+        int uzunKenar=input.nextInt();
+
+        int alan1=alanHesapla(kenar,kenar);
+        System.out.println("Karenin alanı: " +alan1);
+        int alan2=alanHesapla(kisaKenar,uzunKenar);
+        System.out.println("Dikdörtgenin alanı: " +alan2);
+
+        int cevre1=cevreHesapla(kenar,kenar);
+        System.out.println("Karenin çevresi: " +cevre1);
+        int cevre2=cevreHesapla(kisaKenar,uzunKenar);
+        System.out.println("Dikdörtgenin çevresi:" +cevre2);
+
+
     }
-}
+    public static int alanHesapla (int kenar1, int kenar2)
+    {
+        return kenar1*kenar2;
+    }
+        public static int cevreHesapla(int kisaKenar,int uzunKenar){
+            return 2 * (kisaKenar + uzunKenar);
+
+        }
+
+    }
+
