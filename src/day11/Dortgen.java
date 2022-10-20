518f545e729f39e82cd6f718bce66e81028cda3a
ademgencer <ademyasargencer@gmail.com>
2022-10-20 13:02:37 +0300

    Adem Gencer day11 Dortgen ödevi.

--- src/day11/Dortgen.java
+++ src/day11/Dortgen.java
@@ -1,5 +1,7 @@
 package day11;
 
+import java.util.Scanner;
+
 public class Dortgen {
     public static void main(String[] args) {
         //TODO: Dortgen programını geliştiriniz.
@@ -17,5 +19,33 @@ public class Dortgen {
             6. Elde edilen toplam alan hesaplanıp ekranda gösterilecektir.
             7. Elde edilen toplam çevre ekranda gösterilecektir.
          */
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Karenin kenarını giriniz: ");
+        int kenar = scan.nextInt();
+        System.out.println("Dikdörtgenin kısa kenarını giriniz: ");
+        int kisaKenar = scan.nextInt();
+        System.out.println("Dikdörtgenin uzun kenarını giriniz: ");
+        int uzunKenar = scan.nextInt();
+
+        int kareAlan = alanHesapla(kenar,kenar);
+        int dikdortgenAlan = alanHesapla(kisaKenar,uzunKenar);
+
+        int kareCevre = cevreHesapla(kenar, kenar);
+        int dikdortgenCevre = cevreHesapla(kisaKenar, uzunKenar);
+
+        System.out.println("Toplam alan: " + (kareAlan + dikdortgenAlan));
+        System.out.println("Toplam çevre: " + (kareCevre + dikdortgenCevre));
+
+    }
+
+    public static int alanHesapla(int k1, int k2){
+
+        return k1*k2;
+    }
+
+    public static int cevreHesapla(int k1, int k2){
+
+        return 2*(k1+k2);
     }
 }
