40e7074529ed30e5dcf9b0c0a644c0a3ede2e60c
nizamimtest <nizamimtest@gmail.com>
2022-10-24 21:11:45 +0400

    Nizami Muradov Dortgen Odevini Yapti

--- src/day11/Dortgen.java
+++ src/day11/Dortgen.java
@@ -1,5 +1,7 @@
 package day11;
 
+import java.util.Scanner;
+
 public class Dortgen {
     public static void main(String[] args) {
         //TODO: Dortgen programını geliştiriniz.
@@ -17,5 +19,27 @@ public class Dortgen {
             6. Elde edilen toplam alan hesaplanıp ekranda gösterilecektir.
             7. Elde edilen toplam çevre ekranda gösterilecektir.
          */
+        Scanner input = new Scanner(System.in);
+        System.out.print("Kareninin kenarini giriniz. ");
+        int kenar = input.nextInt();
+        System.out.print("Dikdortgenin uzun kenarini giriniz. ");
+        int uzun = input.nextInt();
+        System.out.print("Dikdortgenin kisa kenarini giriniz. ");
+        int kisa = input.nextInt();
+        int alan1=alanHesapla(kenar,kenar);
+        int alan2=alanHesapla(uzun,kisa);
+
+        int cevre1=cevreHesapla(kenar,kenar);
+        int cevre2=cevreHesapla(kisa,uzun);
+        System.out.println("TOPLAM ALAN = "+(alan2+alan2));
+        System.out.println("TOPLAM CEVRE= "+(cevre1+cevre2));
+
+
+    }
+    public static int alanHesapla(int kenar1,int kenar2){
+        return kenar1*kenar2;
+    }
+    public static int cevreHesapla(int kenar1,int kenar2){
+        return 2*kenar1*kenar2;
     }
 }
