9067bf3210bbbc7bd0c6dcc4508ad99e0d8608d8
isakrc52 <114700918+isakrc52@users.noreply.github.com>
2022-11-02 18:30:23 +0300

    Max Min Number

--- src/MaxMix.java
+++ src/MaxMix.java
@@ -6,7 +6,7 @@ public class MaxMix {
         int [] series = new int[10];
         Scanner input = new Scanner(System.in);
 
-        int max;
+        int max,min;
 
         for (int i = 0; i <10; i++) {
             System.out.print((i+1)+".ci sayıyı giriniz:");
@@ -16,8 +16,14 @@ public class MaxMix {
         for (int i = 1; i < 10; i++) {
            if (series[i]>max) max=series[i];
         }
+        min=series [0];
+        for (int i = 1; i <10; i++) {
+            if (series[i]<min) min=series[i];
+        }
 
         System.out.print("max number oh the numbers listed="+max);
+        System.out.println();
+        System.out.print("min number oh the numbers listed="+min);
 
 
 
