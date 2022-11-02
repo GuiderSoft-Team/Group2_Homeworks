ddea677b5c2a6cd8682171b1a2e7e3d326fd49e6
Burcu Uğur <burcuturan737@gmail.com>
2022-11-02 15:50:15 +0300

    Burcu Uğur- Max-Min homework is done.

--- src/day20/MaxMin.java
+++ src/day20/MaxMin.java
@@ -5,26 +5,33 @@ import java.util.Scanner;
 public class MaxMin {
     public static void main(String[] args) {
 
-            int [] sayilar= new int[10];
-            int minimumDeger=0;
-            int maksimumDeger=0;
-            for (int i=0; i< sayilar.length; i++){
-                Scanner input = new Scanner(System.in);
-                System.out.print("Lütfen " + (i+1)+".sayıyı giriniz: " );
-                sayilar[i]=input.nextInt();
-                minimumDeger=sayilar[0];
-                maksimumDeger=sayilar[0];
 
-                if (sayilar[i]<minimumDeger){
-                    minimumDeger=sayilar[i];
+            int[] sayilar = new int[10];
+            int minimumDeger;
+            int maksimumDeger;
+            for (int i = 0; i < sayilar.length; i++) {
+                Scanner input = new Scanner(System.in);
+                System.out.print("Lütfen " + (i+1) + ".sayıyı giriniz: ");
+                sayilar[i] = input.nextInt();
+            }
+
+        minimumDeger = sayilar[0];
+        maksimumDeger = sayilar[0];
+
+        for (int i = 0; i <sayilar.length ; i++) {
+
+
+                if (sayilar[i] < minimumDeger) {
+                    minimumDeger = sayilar[i];
                 }
-                if (sayilar[i]>maksimumDeger) {
+                if (sayilar[i] > maksimumDeger) {
                     maksimumDeger = sayilar[i];
                 }
 
-            }
+        }
 
-            System.out.println("Minimum Değer: " +minimumDeger);
-            System.out.print("Maksimum Değer: " +maksimumDeger);
+            System.out.println("Minimum Değer: " + minimumDeger);
+            System.out.print("Maksimum Değer: " + maksimumDeger);
+        }
     }
-}
+
