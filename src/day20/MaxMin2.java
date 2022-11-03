b6d9846631aba5fad5e2158ad71e2f39124b612d
Mümin <mozdamar90@hotmail.com>
2022-11-03 17:15:54 +0300

    Mümin : TaskArray homework has done.

--- src/day20/MaxMin2.java
+++ src/day20/MaxMin2.java
@@ -6,32 +6,37 @@ public class MaxMin2 {
     public static void main(String[] args) {
 
 
-        System.out.print("Number : ");
+        System.out.println("Lütfen 10 Adet Sayı Giriniz : ");
+        Scanner input = new Scanner(System.in);
+        int[] sayilar = new int[10];
+        for (int i = 0; i < sayilar.length; i++) {
+            System.out.print(i + 1 + ". Sayı :");
+            sayilar[i] = input.nextInt();
 
-        int[] sayilar=new int[10];
-
-        int max=sayilar[0];
-        int min=sayilar[0];
+        }
 
 
-        for (int i = 0; i <sayilar.length ; i++) {
-            numbers(sayilar[i]);
+        int max = sayilar[0];
+        int min = sayilar[0];
 
-            if (sayilar[0]>max){
-                max=sayilar[i];
-            }else if (sayilar[0]<min){
-                min=sayilar[i];
+
+        for (int i = 1; i < sayilar.length; i++) {
+
+            if (max < sayilar[i]) {
+                max = sayilar[i];
+            }
+            if (min > sayilar[i]) {
+                min = sayilar[i];
             }
 
         }
         System.out.println();
 
-        System.out.printf("Max sayi=%5d\nMin sayi=%5d",max,min);
+        System.out.printf("Max sayi=%5d\nMin sayi=%5d", max, min);
 
 
-    }
 
-    public static int numbers(int number){
-        return new Scanner(System.in).nextInt();
     }
 }
+
+
---
+++ src/day21/TaskArray.java
@@ -0,0 +1,35 @@
+package day21;
+
+public class TaskArray {
+    public static void main(String[] args) {
+        /*String[ ] data={"Ahmet","Fizik","Şengül","Kimya","Hakan","Matematik","Feraye","Biyoloji","Cengiz","Felsefe","Salih","Elektronik","Sezen","Bilgisayar","Hasan","Fizik","Sezgin","Biyoloji" };
+
+        Yukarıdaki dizi verildiğine göre;
+        Adı Bölümü
+        Ahmet Fizik
+
+        biçiminde ekran çıktısı veren bir Java programı yazınız.
+
+        Not: GitHub'da Groups homework üzerinden gönderiniz.
+*/
+        String[] data = {"Ahmet", "Fizik", "Şengül", "Kimya", "Hakan", "Matematik", "Feraye", "Biyoloji",
+                "Cengiz", "Felsefe", "Salih", "Elektronik", "Sezen", "Bilgisayar", "Hasan", "Fizik",
+                "Sezgin", "Biyoloji"};
+
+        System.out.println("\tAdı-----------------Bölümü");
+
+
+        for (int i = 0; i < data.length; i += 2) {
+            System.out.printf("\t%s\t\t\t\t%s",data[i],data[i+1]);
+            System.out.println();
+        }
+
+    }
+}
+
+
+
+
+
+
+
