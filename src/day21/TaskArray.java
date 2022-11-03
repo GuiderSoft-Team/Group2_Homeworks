4ca0b2d7562e26e7c651d82d392ca2dbf33ff186
Mümin <mozdamar90@hotmail.com>
2022-11-03 20:14:25 +0300

    Mümin : TaskArray homework has done.

--- src/day21/TaskArray.java
+++ src/day21/TaskArray.java
@@ -2,16 +2,9 @@ package day21;
 
 public class TaskArray {
     public static void main(String[] args) {
-        /*String[ ] data={"Ahmet","Fizik","Şengül","Kimya","Hakan","Matematik","Feraye","Biyoloji","Cengiz","Felsefe","Salih","Elektronik","Sezen","Bilgisayar","Hasan","Fizik","Sezgin","Biyoloji" };
 
-        Yukarıdaki dizi verildiğine göre;
-        Adı Bölümü
-        Ahmet Fizik
 
-        biçiminde ekran çıktısı veren bir Java programı yazınız.
 
-        Not: GitHub'da Groups homework üzerinden gönderiniz.
-*/
         String[] data = {"Ahmet", "Fizik", "Şengül", "Kimya", "Hakan", "Matematik", "Feraye", "Biyoloji",
                 "Cengiz", "Felsefe", "Salih", "Elektronik", "Sezen", "Bilgisayar", "Hasan", "Fizik",
                 "Sezgin", "Biyoloji"};
