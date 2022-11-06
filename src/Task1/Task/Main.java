81251b5a626b7dbdcfdbb6174399ba2e1259e168
Baristopcu08 <112826011+Baristopcu08@users.noreply.github.com>
2022-11-06 11:41:32 +0300

    Update and rename src/Deneme1/Main.java to src/Task1/Task/Main.java

---
+++ src/Task1/Task/Main.java
@@ -0,0 +1,12 @@
+package Deneme1;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        /*
+        bu soruyu çözünüz
+        */
+        
+        //System.out.println("Deneme1");
+    }
+}
